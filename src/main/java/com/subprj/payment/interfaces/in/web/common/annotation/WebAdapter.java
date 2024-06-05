c4eb4d3560a795852dcbb5838916765f5dba7afb
kiomnd2 <kiomnd2@gmail.com>
2024-06-06 01:39:23 +0900

    결제 화면 및 결제 이벤트 생성

---
+++ src/main/java/com/subprj/payment/interfaces/in/web/common/annotation/WebAdapter.java
@@ -0,0 +1,11 @@
+package com.subprj.payment.interfaces.in.web.common.annotation;
+
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Component
+public @interface WebAdapter {
+}
